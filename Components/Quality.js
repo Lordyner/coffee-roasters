3940dce075580441494f84ee0f10e76b9d9052c3
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-03-02 14:02:44 +0100

    Add for review

--- Components/Quality.js
+++ Components/Quality.js
@@ -14,9 +14,11 @@ const Quality = () => {
         <section className={classes.quality}>
             <div className={classes.wrapper}>
                 <Image src={isMobileResolution ? imageQualityMobile : isTabletResolution ? imageQualityTablet : imageQualityDesktop} className={classes.qualityCoffeeImg} alt='coffee with a flower draw inside' />
-                <div className={classes.content}>
-                    <h2>Uncompromising quality</h2>
-                    <p className={classes.description}>Although we work with growers who pay close attention to all stages of harvest and processing, we employ, on our end, a rigorous quality control program to avoid over-roasting or baking the coffee dry. Every bag of coffee is tagged with a roast date and batch number. Our goal is to roast consistent, user-friendly coffee, so that brewing is easy and enjoyable.</p>
+                <div className={classes.bg}>
+                    <div className={classes.content}>
+                        <h2>Uncompromising quality</h2>
+                        <p className={classes.description}>Although we work with growers who pay close attention to all stages of harvest and processing, we employ, on our end, a rigorous quality control program to avoid over-roasting or baking the coffee dry. Every bag of coffee is tagged with a roast date and batch number. Our goal is to roast consistent, user-friendly coffee, so that brewing is easy and enjoyable.</p>
+                    </div>
                 </div>
             </div>
         </section>
