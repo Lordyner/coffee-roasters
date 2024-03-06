1d63b88fb8994005155b44f9c9edde8d6289a186
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-03-06 18:15:10 +0100

    Fix hero section on subscribe page

--- Components/SubscribeHero.js
+++ Components/SubscribeHero.js
@@ -4,7 +4,7 @@ const SubscribeHero = () => {
     return (
         <section className={`${classes.subscribe} max-width`}>
             <div className={classes.wrapper}>
-                <h1>Create Plan</h1>
+                <h1>Create a Plan</h1>
                 <p className={classes.description}>Build a subscription plan that best fits your needs. We offer an assortment of the best artisan coffees from around the globe delivered fresh to your door.</p>
             </div>
         </section>
